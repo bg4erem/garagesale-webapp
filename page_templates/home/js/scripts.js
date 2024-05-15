f85fb8e89cce6fd7b7333436df265f3b7032a2b6
Andrew C <bg4erem@gmail.com>
2024-05-15 23:50:11 +0500

    display item in home if not sold

--- page_templates/home/js/scripts.js
+++ page_templates/home/js/scripts.js
@@ -10,42 +10,44 @@ async function load_and_render_items() {
     let items = await fetch("/api/v1/items").then( resp => { return resp.json() })
 
     items.forEach(item => {
-        let itemCard = document.createElement("div");
+        if (!item.sold) {
+            let itemCard = document.createElement("div");
 
-        let itemPictureSrc = "https://dummyimage.com/450x300/dee2e6/6c757d.jpg";
-        if (item.photos.length > 0) { itemPictureSrc = item.photos[0] }
+            let itemPictureSrc = "https://dummyimage.com/450x300/dee2e6/6c757d.jpg";
+            if (item.photos.length > 0) { itemPictureSrc = item.photos[0] }
 
-        itemCard.className = "col mb-5";
-        let itemPriceHTML = `${item.price} ${item.price_curr}`;
-        if (item.full_price) { 
-            itemPriceHTML = `
-                <span class="text-muted text-decoration-line-through">${item.full_price} ${item.price_curr}</span>
-                ${item.price} ${item.price_curr}
-            ` 
-        }
-        itemCard.innerHTML = `
-            <div class="card h-100">
-                <!-- Sale badge-->
-                <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-                <!-- Product image-->
-                <img class="card-img-top" src="${itemPictureSrc}" alt="..." />
-                <!-- Product details-->
-                <div class="card-body p-4">
-                    <div class="text-center">
-                        <!-- Product name-->
-                        <h5 class="fw-bolder">${item.title}</h5>
-                        <!-- Product price-->
-                        ${itemPriceHTML}
+            itemCard.className = "col mb-5";
+            let itemPriceHTML = `${item.price} ${item.price_curr}`;
+            if (item.full_price) { 
+                itemPriceHTML = `
+                    <span class="text-muted text-decoration-line-through">${item.full_price} ${item.price_curr}</span>
+                    ${item.price} ${item.price_curr}
+                ` 
+            }
+            itemCard.innerHTML = `
+                <div class="card h-100">
+                    <!-- Sale badge-->
+                    <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+                    <!-- Product image-->
+                    <img class="card-img-top" src="${itemPictureSrc}" alt="..." />
+                    <!-- Product details-->
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <!-- Product name-->
+                            <h5 class="fw-bolder">${item.title}</h5>
+                            <!-- Product price-->
+                            ${itemPriceHTML}
+                        </div>
+                    </div>
+                    <!-- Product actions-->
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/details/${item['_id']}">Подробнее</a></div>
                     </div>
                 </div>
-                <!-- Product actions-->
-                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="/details/${item['_id']}">Подробнее</a></div>
-                </div>
-            </div>
-        `;
+            `;
 
-        itemsContainer.appendChild(itemCard);
+            itemsContainer.appendChild(itemCard);
+        }
     })
 }
 
