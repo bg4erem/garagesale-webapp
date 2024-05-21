418dd35a1613538d93e9203a8ca7aae1d194ed6a
Andrew C <bg4erem@gmail.com>
2024-05-21 19:03:13 +0500

    geopoint renamed to coordinates

--- ip_lookup.py
+++ ip_lookup.py
@@ -12,14 +12,14 @@ async def lookup_ip(ip: str) -> dict | None:
                 ip_info = await response.json()
         if type(ip_info) is dict and ip_info.get("ip"):
             if ip_info.get("longitude") and ip_info.get("latitude"):
-                ip_info["geopoint"] = [ip_info.get("longitude"), ip_info.get("latitude")]
+                ip_info["coordinates"] = [ip_info.get("longitude"), ip_info.get("latitude")]
             ip_info["_id"] = ip
             await ips_collection.insert_one(ip_info)
         else:
             ip_info = None
     else:
-        if not ip_stored.get("geopoint") and ip_stored.get("longitude") and ip_stored.get("latitude"):
-            ip_stored["geopoint"] = [ip_stored.get("longitude"), ip_stored.get("latitude")]
+        if not ip_stored.get("coordinates") and ip_stored.get("longitude") and ip_stored.get("latitude"):
+            ip_stored["coordinates"] = [ip_stored.get("longitude"), ip_stored.get("latitude")]
             await ips_collection.update_one({"_id":ip}, {"$set":ip_stored})
         ip_info = ip_stored
 
