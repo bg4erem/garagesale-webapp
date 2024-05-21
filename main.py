db6362463efd56863386979d11f22346ccc20e50
Andrew C <bg4erem@gmail.com>
2024-05-21 13:03:33 +0500

    fixing several tasks in bg_tasks of http middleware

--- main.py
+++ main.py
@@ -23,10 +23,12 @@ async def add_process_time_header(request: Request, call_next):
     print(f"{request.client.host}, {request.url.path}, {round(process_time, 3)}, {response.status_code}")
 
     log = await request_log(request, response, process_time)
+    async def log_request(log):
+        await requests_collection.insert_one(log)
+        if opensearch_client:
+            await opensearch_client.index(INDEX_REQUESTS, log)
     bg_tasks = BackgroundTasks()
-    bg_tasks.add_task(requests_collection.insert_one, log)
-    if opensearch_client:
-        bg_tasks.add_task(opensearch_client.index, INDEX_REQUESTS, log)
+    bg_tasks.add_task(log_request, log)
 
     response.background = bg_tasks
 
