7a3cf5d9df99429a4a4523bb605f8617ede4a5d5
Andrew C <bg4erem@gmail.com>
2024-05-22 21:40:47 +0500

    added user agent parse

--- models/stats.py
+++ models/stats.py
@@ -2,6 +2,7 @@ from typing import TypedDict
 from datetime import datetime, timezone
 from fastapi import Request, Response
 from pydantic import BaseModel, Field
+import user_agents
 
 def get_now_utc() -> datetime:
     return datetime.now(timezone.utc)
@@ -25,9 +26,31 @@ async def request_log(request: Request, response: Response, process_time: float)
         status_code: int
         user_agent: str|None
         ip_lookup: dict
-        location: list[float]|None
+        user_agent_parse: dict|None
+        cookies: dict|None
 
     ip_lookup = await lookup_ip(request.client.host)
+    user_agent = request.headers.get("User-Agent")
+
+    user_agent_parse = None
+    if user_agent:
+        user_agent_parse = {}
+        try:
+            user_agent_parse["is_bot"] = user_agents.parse(user_agent).is_bot
+            user_agent_parse["is_email_client"] = user_agents.parse(user_agent).is_email_client
+            user_agent_parse["is_mobile"] = user_agents.parse(user_agent).is_mobile
+            user_agent_parse["is_pc"] = user_agents.parse(user_agent).is_pc
+            user_agent_parse["is_tablet"] = user_agents.parse(user_agent).is_tablet
+            user_agent_parse["base_string"] = str(user_agents.parse(user_agent))
+            user_agent_parse["os_family"] = user_agents.parse(user_agent).os.family
+            user_agent_parse["os_version_string"] = user_agents.parse(user_agent).os.version_string
+            user_agent_parse["browser_family"] = user_agents.parse(user_agent).browser.family
+            user_agent_parse["browser_version_string"] = user_agents.parse(user_agent).browser.version_string
+            user_agent_parse["device_family"] = user_agents.parse(user_agent).device.family
+            user_agent_parse["device_brand"] = user_agents.parse(user_agent).device.brand
+            user_agent_parse["device_model"] = user_agents.parse(user_agent).device.model
+        except:
+            ...
 
     request_log: RequestLog = {
         "timestamp": datetime.now(timezone.utc),
@@ -37,7 +60,9 @@ async def request_log(request: Request, response: Response, process_time: float)
         "process_time": process_time,
         "status_code": response.status_code,
         "ip_lookup": ip_lookup,
-        "user_agent": request.headers.get("User-Agent"),
+        "user_agent": user_agent,
+        "user_agent_parse": user_agent_parse,
+        "cookies":request.cookies,
     }
 
     return request_log
